8f9f648a81b373e75b84869b768e1eb1322ea5ef
YdeaGames <ydeagames@gmail.com>
2019-07-17 12:21:06 +0900

    へび

--- MyGame.cpp
+++ MyGame.cpp
@@ -22,19 +22,7 @@ void MyGame::Initialize(GameContext& context)
 	m_model = DirectX::Model::CreateFromCMO(context.GetDR().GetD3DDevice(), L"Resources/Models/bone.cmo", fxFactory);
 
 	Transform* lastTransform = nullptr;
-	{
-		auto transform = std::make_shared<Transform>();
-		transform->parent = (lastTransform);
-		m_transform.push_back(transform);
-		lastTransform = transform.get();
-	}
-	{
-		auto transform = std::make_shared<Transform>();
-		transform->parent = (lastTransform);
-		transform->localPosition = Vector3::Up;
-		m_transform.push_back(transform);
-		lastTransform = transform.get();
-	}
+	for (int i = 0; i < 10; i++)
 	{
 		auto transform = std::make_shared<Transform>();
 		transform->parent = (lastTransform);
@@ -55,35 +43,16 @@ void MyGame::Update(GameContext& context)
 	// ‰ñ“]
 	float speed = 5.f * float(context.GetTimer().GetElapsedSeconds()) * 60.f;
 
+	for (int i = 0; i < 10; i++)
 	{
 		if (Input::GetKey(Keyboard::Keys::W))
-			m_transform[0]->localRotation *= Quaternion::CreateFromAxisAngle(Vector3::UnitX, XMConvertToRadians(-speed));
+			m_transform[i]->localRotation *= Quaternion::CreateFromAxisAngle(Vector3::UnitX, XMConvertToRadians(-speed));
 		if (Input::GetKey(Keyboard::Keys::S))
-			m_transform[0]->localRotation *= Quaternion::CreateFromAxisAngle(Vector3::UnitX, XMConvertToRadians(speed));
+			m_transform[i]->localRotation *= Quaternion::CreateFromAxisAngle(Vector3::UnitX, XMConvertToRadians(speed));
 		if (Input::GetKey(Keyboard::Keys::D))
-			m_transform[0]->localRotation *= Quaternion::CreateFromAxisAngle(Vector3::UnitZ, XMConvertToRadians(-speed));
+			m_transform[i]->localRotation *= Quaternion::CreateFromAxisAngle(Vector3::UnitZ, XMConvertToRadians(-speed));
 		if (Input::GetKey(Keyboard::Keys::A))
-			m_transform[0]->localRotation *= Quaternion::CreateFromAxisAngle(Vector3::UnitZ, XMConvertToRadians(speed));
-	}
-	{
-		if (Input::GetKey(Keyboard::Keys::Up))
-			m_transform[1]->localRotation *= Quaternion::CreateFromAxisAngle(Vector3::UnitX, XMConvertToRadians(-speed));
-		if (Input::GetKey(Keyboard::Keys::Down))
-			m_transform[1]->localRotation *= Quaternion::CreateFromAxisAngle(Vector3::UnitX, XMConvertToRadians(speed));
-		if (Input::GetKey(Keyboard::Keys::Right))
-			m_transform[1]->localRotation *= Quaternion::CreateFromAxisAngle(Vector3::UnitZ, XMConvertToRadians(-speed));
-		if (Input::GetKey(Keyboard::Keys::Left))
-			m_transform[1]->localRotation *= Quaternion::CreateFromAxisAngle(Vector3::UnitZ, XMConvertToRadians(speed));
-	}
-	{
-		if (Input::GetKey(Keyboard::Keys::I))
-			m_transform[2]->localRotation *= Quaternion::CreateFromAxisAngle(Vector3::UnitX, XMConvertToRadians(-speed));
-		if (Input::GetKey(Keyboard::Keys::K))
-			m_transform[2]->localRotation *= Quaternion::CreateFromAxisAngle(Vector3::UnitX, XMConvertToRadians(speed));
-		if (Input::GetKey(Keyboard::Keys::L))
-			m_transform[2]->localRotation *= Quaternion::CreateFromAxisAngle(Vector3::UnitZ, XMConvertToRadians(-speed));
-		if (Input::GetKey(Keyboard::Keys::J))
-			m_transform[2]->localRotation *= Quaternion::CreateFromAxisAngle(Vector3::UnitZ, XMConvertToRadians(speed));
+			m_transform[i]->localRotation *= Quaternion::CreateFromAxisAngle(Vector3::UnitZ, XMConvertToRadians(speed));
 	}
 }
 
